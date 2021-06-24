537c6984eae29d40656b262c6cf889728b7e6d54
Deborah Lee <67073930+bora12344@users.noreply.github.com>
2021-06-24 14:47:19 -0500

    JS file

---
+++ main.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// upload items from json file by using fetch
+function loadItems() {
+  return fetch('data/data.json')
+  .then(response => response.json())
+  .then(json => json.items);
+}
+
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item));
+}
+
+
+// create HTML list item from the given data item 
+function createHTMLString(item) {
+    return `
+      <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+      `;
+}
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null){
+    return;
+  } else {
+    const filtered = items.filter(item => item[key] === value);
+    console.log(filtered);
+    displayItems(filtered);
+    //displayItems(items.filter(item => item[key] === value))
+  }
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
+  }
+
+
+
+// make a default view without clicking buttons
+// main
+loadItems()
+  .then(items => {
+    console.log(items);
+    displayItems(items);
+    setEventListeners(items)
+  })
+  .catch(console.log);
